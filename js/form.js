81fea432a95d15fff106f826411bf30e989fb2c2
fitty0930 <martinaguirre08@gmail.com>
2021-05-29 19:04:14 -0300

    agregado boton mejorado

--- js/form.js
+++ js/form.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     let pp = document.getElementById('personalplaceholder');
     let textarea = document.getElementById('textarea');
 
+    document.getElementById("button").addEventListener("click", function(event) {
+        event.preventDefault()
+    });
+
     textarea.addEventListener('mousedown', () => {
         if (textarea.value == "") {
             pp.classList.remove("ocultar")
